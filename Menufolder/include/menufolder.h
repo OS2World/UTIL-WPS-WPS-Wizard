c4a4fa2e85b0532129ef8e402503a4e42fc10922
cinc <cinc@0dff73f2-a111-0410-a3d5-b7b0f6812f68>
2008-01-05 12:09:21 +0000

    New include file holding menufolder specific defines

---
+++ Menufolder/include/menufolder.h
@@ -0,0 +1,26 @@
+    #ifndef BS_NOTEBOOKBUTTON
+        #define BS_NOTEBOOKBUTTON          8L
+    #endif
+
+
+#define ID_CLASSPAGENAME        1000
+#define ID_GROUPBOXTEXT         1001
+#define ID_FOLDERTITLE          1002
+
+#define ID_MENUFOLDERCLASSNAME     2003
+
+#define MENUFOLDER_CLASSNAME       "CWMenuFolder"
+
+/* Key where to save instance data bit */
+#define IDKEY_ENABLED 4100
+
+#define ID_MENUENABLED     0x00000001
+#define ID_HIDECLASSPAGE   0x00000002
+
+/* Setupstring */
+#define SETUP_HIDECLASSPAGE  "HIDECLASSPAGE"
+#define SETUP_CLEARMENUCACHE "CLEARMENUCACHE"
+
+
+
+
