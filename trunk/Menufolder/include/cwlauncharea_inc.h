07b4207bd3220d984d55c4b0e80baa1c984e555b
cinc <cinc@0dff73f2-a111-0410-a3d5-b7b0f6812f68>
2008-01-20 10:56:11 +0000

    Include file holding CWLaunchArea specific defines and stuff

---
+++ trunk/Menufolder/include/cwlauncharea_inc.h
@@ -0,0 +1,19 @@
+/*
+  This file is (C) Chris Wohlgemuth 2008
+
+  This file is copyrighted.
+
+  For licensing contact cinc-ml@netlabs.org
+
+  No use or copying without prior permission.
+
+*/
+
+/* Define the color the launcharea and the buttons on it will have. */
+#define CLR_LAUNCHAREA SYSCLR_DIALOGBACKGROUND
+
+#define CW_LAUNCHAREA       "CW_LAUNCHAREA"
+#define LAUNCHAREA_DATASIZE 4
+
+/* Used for open view handling in the WPS. */
+#define VIEW_LAUNCHAREA  WPMENUID_USER+0x1000
